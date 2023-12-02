e9f1d0b9f7ceb46631bcdd03daa44fd44c0cc320
Yasse7 <yasserjerrari2001@gmail.com>
2023-12-02 22:42:17 +0100

    first-commit

---
+++ Gateway-Service/src/main/java/com/ll/exam/GatewayService/GatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.GatewayService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+}
---
+++ Order-Service/src/test/java/com/ll/exam/OrderService/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.OrderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
